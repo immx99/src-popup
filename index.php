8abe52cb4825e7cbaa9a1daf7bc7d91465523651
Imam Subarkah <72020984+immx99@users.noreply.github.com>
2021-11-27 11:04:38 +0700

    Popup untuk service item

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/esm/popper.min.js"></script>
   <!--script src="js/popper.min.js"></script -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
   <link rel="stylesheet" type="text/css" href="./css/style.css">
 
